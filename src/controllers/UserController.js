fc5c62d649e32c23eb82cd698c9f66dc6be04855
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-03-31 14:59:35 -0300

    Criando a rota e o metodo delete

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -53,6 +53,28 @@ class UserController {
       return res.json(null);
     }
   }
+
   // delete
+  async delete(req, res) {
+    try {
+      // verificação se o ID é valido
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não enviado.'],
+        });
+      }
+      // encontando usuario pelo ID
+      const user = await User.findByPk(req.params.id);// mostrando de acordo com a Primary Key do Usuario
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuario inexistente mermao.'],
+        });
+      }
+      await user.destroy();
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
 }
 export default new UserController();
