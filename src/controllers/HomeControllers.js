72c642080a51be96b24d1e2397fa55db14d5ee67
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-04-02 06:00:54 -0300

    criando migration, controllers e rota de aluno

--- src/controllers/HomeControllers.js
+++ src/controllers/HomeControllers.js
@@ -1,18 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Geronimo',
-      sobrenome: 'Olanda',
-      email: 'geronimoolandaateste@gmail.com',
-      idade: 23,
-      peso: 107,
-      altura: 1.85,
-    });
-    res.json({
-      novoAluno,
-    });
+    res.json('index');
   }
 }
 export default new HomeController();
