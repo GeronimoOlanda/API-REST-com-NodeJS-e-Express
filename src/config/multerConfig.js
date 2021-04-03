b0e83d58cde4b72f055284ca712846b28ef7c442
GeronimoOlanda <geronimo.olanda@outlook.com>
2021-04-03 06:35:34 -0300

    otimizando upload de arquivos e refatorando

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -5,6 +5,13 @@ const aleatorio = () => Math.floor(Math.random() * 10000 + 10000);
 
 // salvando no servidor em alguma pasta
 export default {
+
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+      return cb(new multer.MulterError('Arquivo precisa ser PNG ou JPEG'));
+    }
+    return cb(null, true);
+  },
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, resolve(__dirname, '..', '..', 'uploads'));
